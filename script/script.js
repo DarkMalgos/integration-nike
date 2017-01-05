5ab869e673e6b52432dc9ca671ab48bd84187832
Florian SAUMUROT <florian.saumurot@gmail.com>
2017-01-05 18:42:00 +0100

    version 0.5

--- script/script.js
+++ script/script.js
@@ -9,23 +9,40 @@
 =============================================================*/
 
 
-var h = document.getElementById("heart"),
+var cookie_cart = getCookie("cart"),
+    cookie_fav = getCookie("fav"),
+    h = document.getElementById("heart"),
     fav = false,
     pl = document.getElementById("product-left"),
     sp = document.getElementById("shoe-pannel"),
+    c = document.getElementById("caract"),
     cs = document.getElementById("m1"),
     ucs = document.getElementById("m2"),
+    nb_cart = 0,
+    cart = document.getElementById("cart"),
+    add1 = document.getElementById("add1"),
+    add2 = document.getElementById("add2"),
     body = document.getElementsByTagName("body")[0],
     fp = document.getElementById("fix-pannel");
+
+if (cookie_cart != null){
+    add_remove_to_cart();
+}
+
+if (cookie_fav != null){
+    check_fav();
+}
 
 function check_fav() {
     if (fav == true) {
         fav = false;
         h.setAttribute('src', "images/heart-1.png");
-    }
-    else {
+        eraseCookie("fav");
+    } else {
+        setCookie("fav", "true", 30);
         fav = true;
         h.setAttribute('src', "images/heart-2.png");
+        setCookie("fav", "true", 30);
     }
 }
 
@@ -33,6 +50,7 @@ h.addEventListener('click', check_fav);
 
 function go_colored_shoes(){
     pl.style.background = "url('images/top-background-1.png')";
+    c.style.background = "url('images/shoes.png')";
     sp.style.background = "url('images/top-background-1.png')";
     sp.style.backgroundSize = "100% 100%";
 }
@@ -41,11 +59,31 @@ cs.addEventListener('click', go_colored_shoes);
 
 function go_uncolored_shoes(){
     pl.style.background = "url('images/top-background-2.png')";
+    c.style.background = "url('images/shoes2.png')";
     sp.style.background = "url('images/top-background-2.png')";
         sp.style.backgroundSize = "100% 100%";
 }
 
 ucs.addEventListener('click', go_uncolored_shoes);
+
+function add_remove_to_cart() {
+    if (add1.innerHTML == "ADD TO CART") {
+        ++nb_cart;
+        add1.innerHTML = "REMOVE TO CART";
+        add2.innerHTML = add1.innerHTML;
+        cart.innerHTML = nb_cart;
+        setCookie("cart", nb_cart, 30);
+    } else {
+        --nb_cart;
+        add1.innerHTML = "ADD TO CART";
+        add2.innerHTML = add1.innerHTML;
+        cart.innerHTML = nb_cart;
+        eraseCookie("cart");
+    }
+}
+
+add1.addEventListener('click', add_remove_to_cart);
+add2.addEventListener('click', add_remove_to_cart);
 
 window.onscroll = function(){
     if (document.body.scrollTop >= 0 && document.body.scrollTop < 690){
@@ -61,10 +99,11 @@ window.onscroll = function(){
         fp.style.position = "absolute";
         fp.style.top = "1500px"; 
     }
-        
 }
 
-
-
-
-
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 30
+    });
